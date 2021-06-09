e3805921b93830cfba6b4c9c49c820c4c25a15df
Roman <roman.pohribniak@gmail.com>
2021-06-09 21:00:44 +0300

    add info

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,11 @@
 const { Router } = require("express");
+const Room = require("../schema/room");
 const router = Router();
 
-
+router.post("/getRoomInfo",async  (req,res)=>{
+    const {roomId} = req.body;
+    const resp = await Room.findOne({roomId:roomId});
+    res.send(resp);
+})
 
 module.exports = router;
