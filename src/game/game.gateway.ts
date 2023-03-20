4652260f7b9d1933759bf435ea46a3b7c1f21686
PohribniakRoman <79878288+PohribniakRoman@users.noreply.github.com>
2023-03-20 13:58:22 +0200

    add lobbies join

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,6 +1,35 @@
 import { MessageBody,SubscribeMessage, WebSocketGateway,WebSocketServer, OnGatewayDisconnect} from "@nestjs/websockets";
 import { Server, Socket } from 'socket.io';
 
+class Storage{
+  games:object;
+  constructor() {
+    this.games = {};
+  }
+  deleteGame(id){
+    delete this.games[id];
+  }
+  createGame(id,game){
+    this.games[id]=game;
+  }
+}
+const DB = new Storage();
+class Game {
+  participants:Array<any>;
+  messages:object;
+  constructor(participant,socket){
+    this.participants = [{participant,socket:socket}];
+    this.messages = [];
+  };
+   join(participant,socket){
+    this.participants = this.participants.filter(user => user.participant.name != participant.name)
+    this.participants.push({ participant, socket:socket})
+  }
+  leave(socket){
+    this.participants = this.participants.filter(participant=>participant.socket !== socket)
+  }
+}
+
 @WebSocketGateway()
 export class GameGateway implements OnGatewayDisconnect {
   @WebSocketServer()
@@ -8,8 +37,17 @@ export class GameGateway implements OnGatewayDisconnect {
 
   handleDisconnect(socket:Socket){
   }
-  @SubscribeMessage("ENTER_ROOM")
+  @SubscribeMessage("ENTER")
   enterRoom(socket: Socket,data:any){
-    console.log("ASdasdadas");
+    if(DB.games.hasOwnProperty(data.gameId)){
+      DB.games[data.gameId].join(data.user,socket.id);
+      return null;
+    }
+    this.createRoom(socket,data);
+  }
+
+  createRoom(socket: Socket,data:any){
+      DB.createGame(data.gameId,new Game(data.user,socket.id))
+      this.enterRoom(socket,data);
   }
 }
